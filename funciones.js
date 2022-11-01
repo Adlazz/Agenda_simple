9a0b7e4c552f44d5ddc068b70e5350311cb9fc4b
Adlazz <adrianlazzarini@gmail.com>
2022-11-01 12:04:31 -0300

    add atr id y class a elem cuits y funcion copiar

--- funciones.js
+++ funciones.js
@@ -21,6 +21,7 @@ const mostrarContacto = (parentNode, contacto, db) => {
         let condIvaContacto = document.createElement("p")
         let iconoDelete = document.createElement("span")
         let iconoCopy = document.createElement("span")
+
         // cargar contenido a mostrar dentro de etiquetas
         apellidoContacto.innerHTML = contacto.apellido
         nombreContacto.innerHTML = contacto.nombre
@@ -28,23 +29,37 @@ const mostrarContacto = (parentNode, contacto, db) => {
         condIvaContacto.innerHTML = contacto.cond_iva
         iconoDelete.innerHTML = "delete"
         iconoCopy.innerHTML = "content_copy"
+
         // agregar las clases a las etiquetas
         divContacto.classList.add("contacto")
         iconoDelete.classList.add("material-symbols-outlined", "icono-delete")
         iconoCopy.classList.add("material-symbols-outlined", "icono-copy", "clipboard")
+        
+        // agregar id y class al elemento de cuit 
+        cuitContacto.setAttribute('id', Math.random(1,100))
+        cuitContacto.classList.add("cuits")
+
+        // guardar id en variable para pasar a funcion copiar
+        //cuitId = document.getElementsByClassName("cuits")[0].id;
+        //console.log(cuitId)
+
+        // agregar funcion copiar al clipboard al icono de copiar
+        //iconoCopy.setAttribute('onclick', 'copyToClipboard(cuitId)')
+        
         //Borrar 
         iconoDelete.onclick = () =>{
             db.removeItem(contacto.id)
             window.location.href = "/"
         }
-    
+        
+        // agregar las etiquetas al divContacto
         divContacto.appendChild(apellidoContacto)
         divContacto.appendChild(nombreContacto)
         divContacto.appendChild(cuitContacto)
         divContacto.appendChild(condIvaContacto)
         divContacto.appendChild(iconoDelete)
         divContacto.appendChild(iconoCopy)
-
+        // agregar el div al documento  
         parentNode.appendChild(divContacto)
 }
 
@@ -53,9 +68,17 @@ const mostrarContacto = (parentNode, contacto, db) => {
     const copyData = document.querySelector('.clipboard')
     console.log(copyData)
 
-    buttonCopy.addEventListener('click', function(){
-        p.focus();
-        document.execCommand('selectAll');
+    function copyToClipboard(var1){
+        let val = document.getElementById(var1).innerHTML;
+        const selBox = document.createElement('textarea');
+        selBox.style.position = 'fixed';
+        selBox.style.left = '0';
+        selBox.style.top = '0';
+        selBox.style.opacity = '0';
+        selBox.value = val;
+        document.body.appendChild(selBox);
+        selBox.focus();
+        selBox.select();
         document.execCommand('copy');
-
-    } )
+        document.body.removeChild(selBox);
+      }
